bf16da037a8acaebca707e51cdce0ecf080f3239
MatheusSan99 <mdsmatheus100@gmail.com>
2022-11-16 13:33:15 -0300

    migrations

---
+++ database/migrations/2022_11_16_162708_pacients.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('birth_date');
+            $table->string('cpf');
+            $table->integer('phone_number_list_id');
+            $table->string('cpf');
+            $table->string('adress');
+            $table->string('city');
+            $table->string('responsable_name');
+            $table->string('responsable_cpf');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacients');
+    }
+};
---
+++ database/migrations/2022_11_16_162722_phone_number_list.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('functions', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_number_first');
+            $table->string('phone_number_second');
+            $table->string('phone_number_third');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('functions');
+    }
+};
